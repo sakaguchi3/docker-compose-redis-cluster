888d0a5bb14bfc6f23455cbe17a9b0ca7a58de32
uqw <uqw@outlook.jp>
2018-03-18 21:44:18 +0900

    modify

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if [ "$1" = 'redis-cluster' ]; then
     sleep 10
     echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 0 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002
+#    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 0 192.168.133.50:7000 192.168.133.50:7001 192.168.133.50:7002
     echo "DONE"
 else
   exec "$@"
